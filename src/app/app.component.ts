a7b69b608b8206450392cb54801cb8a9bf9f19dd
karthikeyan <40653337+karthikn-P@users.noreply.github.com>
2018-09-25 12:00:57 +0530

    push
    pushing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'my-app';
+  title = "my first Angular";
+  setData(regform) {
+    regform.preventDefault();
+    console.log(regform);
+    var ob = {
+      k1: regform.target[0].value,
+      k2: regform.target[1].value,
+      k3: regform.target[2].value,
+      k4: regform.target[3].value,
+      k5: regform.target[4].value,
+      k6: regform.target[5].value,
+      k7: regform.target[6].value
+    };
+    localStorage.setItem("object", JSON.stringify(ob));
+  }
+  getData(a) {
+    console.log(a);
+    var user = JSON.parse(localStorage.getItem("object"));
+    return user[a];
+  }
 }
